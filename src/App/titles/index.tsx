a8519d9cbdc5b86a65415fa94397f489a8cd43a4
Angello Obel <angello@obel.co>
2018-11-07 19:33:38 +0300

    changed styling

--- src/App/titles/index.tsx
+++ src/App/titles/index.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import Typography from '@material-ui/core/Typography';
 
 const Titles = () => {
-  return (
-    <Typography variant="h6">
-      Dashboard
-    </Typography>
-  );
+  return <Typography variant="h6" />;
 };
 
 export default Titles;
