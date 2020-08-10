346317d0d12bf60c69f11b71585333ce6d5059ad
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-10 09:16:04 +0000

    Deploy 256-io/dashboard to github.com/256-io/dashboard.git:gh-pages

---
+++ precache-manifest.356d77fcb942f6506ce77dcb665a9743.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "81312d4a81e499095a3d",
+    "url": "/dashboard/static/js/runtime~main.81312d4a.js"
+  },
+  {
+    "revision": "16ce5f95222024f12cc8",
+    "url": "/dashboard/static/js/main.16ce5f95.chunk.js"
+  },
+  {
+    "revision": "62e8fd1cc24fb9084f06",
+    "url": "/dashboard/static/js/1.62e8fd1c.chunk.js"
+  },
+  {
+    "revision": "16ce5f95222024f12cc8",
+    "url": "/dashboard/static/css/main.cfbdfd2e.chunk.css"
+  },
+  {
+    "revision": "7ba4e35ffb7d80de1c4bc6965a9bc21f",
+    "url": "/dashboard/index.html"
+  }
+];
